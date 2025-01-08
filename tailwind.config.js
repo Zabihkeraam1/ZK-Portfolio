715c545098bc45985324a5fd021ca6396c3de3d8
Zabih Keraam <keraamzabih@gmail.com>
2025-01-08 23:29:41 +0430

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-//     './components/**/*.{js,ts,jsx,tsx,mdx}',
-//     './app/**/*.{js,ts,jsx,tsx,mdx}',
-//   ],
-//   theme: {
-//     extend: {
-//       animation: {
-//         'spin-slow': 'spin 20s linear infinite',
-//       },
-//     },
-//   },
-//   plugins: [],
-// }
-
-/** @type {import('tailwindcss').Config} */
 import tailwindcssAnimate from 'tailwindcss-animate';
 export default {
   darkMode: ["class"],
